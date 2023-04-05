10e98490fe13f16aec48e06932221b126759228e
Jakub Rzepinski <rzepa8@gmail.com>
2023-04-05 23:46:26 +0100

    added sorting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
-  ],
+    "./src/**/*.{html,ts}"
+],
   theme: {
     extend: {},
   },
   plugins: [
     
+
   ],
 }
