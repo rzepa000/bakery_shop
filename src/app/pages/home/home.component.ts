7ca0ae664ea29c1fee7ece08482e02033dd9ff6b
Jakub Rzepinski <rzepa8@gmail.com>
2023-07-15 20:55:14 +0100

    added filters and product card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+const ROWS_HEIGHT: {[id:number]: number} = {1: 400, 3:335, 4:350}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,9 +9,14 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   cols=3;
+  rowHeight= ROWS_HEIGHT[this.cols];
+  category: string | undefined;
 
   onColumnsChange(colsNum: number):void {
     this.cols=colsNum;
+    this.rowHeight=ROWS_HEIGHT[this.cols];
   }
-
+  onShowCategory(newCategory: string): void{
+    this.category= newCategory;
+  }
 }
