81d1e8ce7c61d137a6a34ad58e63f207a19102c0
Jakub Rzepinski <rzepa8@gmail.com>
2023-07-24 20:08:22 +0100

    added stripe checkout

--- server/server.js
+++ server/server.js
@@ -8,4 +8,31 @@ app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 app.use(cors({origin: true, credentials: true}));
 
-const stripe=require("stripe")("pk_test_51NWMtHDQEB8Kkh6ZyP3LoUiqDQrNmmlzSekouBSK7a4m7Iaf47KsiDH51jJOKlTYmcCxSN9ulpSUlJLlSbeFUmSS00FHixkPaC");
+const stripe=require("stripe")("");
+
+app.post("/checkout", async (req,res,next)=>{
+    try{
+        const session=await stripe.checkout.sessions.create({
+            line_items: req.body.items.map((item)=> ({
+                price_data:{
+                currency: 'gbp',
+                product_data: {
+                    name: item.name,
+                    images: [item.product]
+                },
+                unit_amount: item.price*100
+            },
+            quantity: item.quantity,
+            })),
+            mode: 'payment',
+            success_url: 'http://localhost:4242/success.html',
+            cancel_url: 'http://localhost:4242/cancel.html',
+        });
+
+        res.status(200).json(session);
+    }catch(error){
+        next(error);
+    }
+});
+
+app.listen(4242, ()=> console.log('app is running 4242 port'))
